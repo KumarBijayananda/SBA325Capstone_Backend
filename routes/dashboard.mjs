5b9e29ca8ed565ba248734123f74349b0abb0778
Kumar Bijayananda <kumarbijayananda@gmail.com>
2025-02-25 00:10:24 -0500

    delete added to dashboard route with id

--- routes/dashboard.mjs
+++ routes/dashboard.mjs
@@ -1,13 +1,13 @@
 //Dependencies
 import express from "express";
 import User from "../models/User.mjs";
+import Archive from '../models/Archive.mjs'
 import auth from "../middleware/auth.mjs";
 
 
 const router = express.Router();
 
 router.get("/", auth, async (req, res) => {
-
   try {
     const user = await User.findById(req.user.id).select("-password");
     res.json(user);
@@ -19,8 +19,24 @@ router.get("/", auth, async (req, res) => {
   }
 });
 
-// router.post('/',(req,res)=>{
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const draftToDel = await User.find({_id: req.user.id,"drafts.id":req.params.id })
+    if(draftToDel){
+      console.log("draftToDel found")
+      await User.deleteOne({_id: req.user.id,"drafts.id":req.params.id })
+      await Archive.deleteMany({draft_id:req.params.id})
+      const user = await User.findById(req.user.id);
+      res.json(user);
+    } else res.send(`No draft with id ${id} found`)
+    
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ errors: [{ msg: "Server Error for dashboard GET" }] });
+  }
+});
 
-// })
 
 export default router;
