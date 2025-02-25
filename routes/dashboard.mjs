3e9f6e1586342b547734619e9bdaca94288e7d05
Kumar Bijayananda <kumarbijayananda@gmail.com>
2025-02-25 12:55:26 -0500

    delete fix for archive to clean up

--- routes/dashboard.mjs
+++ routes/dashboard.mjs
@@ -21,14 +21,26 @@ router.get("/", auth, async (req, res) => {
 
 router.delete("/:id", auth, async (req, res) => {
   try {
-    const draftToDel = await User.find({_id: req.user.id,"drafts.id":req.params.id })
-    if(draftToDel){
-      console.log("draftToDel found")
-      await User.deleteOne({_id: req.user.id,"drafts.id":req.params.id })
+    // const draftToDel = await User.find({_id: req.user.id,"drafts.id":req.params.id })
+    // if(draftToDel){
+    //   console.log("draftToDel found")
+    //   await User.deleteOne({_id: req.user.id,"drafts.id":req.params.id })
+    //   await Archive.deleteMany({draft_id:req.params.id})
+    //   const user = await User.findById(req.user.id);
+    //   res.json(user);
+
+    const user = await User.findOneAndUpdate(
+      { _id: req.user.id, "drafts._id": req.params.id }, // find user and the draft to update
+      {
+        $pull: {
+          drafts:{_id:req.params.id}
+        },
+      });
+
       await Archive.deleteMany({draft_id:req.params.id})
-      const user = await User.findById(req.user.id);
+
       res.json(user);
-    } else res.send(`No draft with id ${id} found`)
+    // } else res.send(`No draft with id ${id} found`)
     
   } catch (error) {
     console.error(error);
